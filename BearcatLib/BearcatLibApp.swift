3b5d7896b3436fec703868ad1f7d755beb89d5a9
Joseph Musenge <81922031+JosephMusenge@users.noreply.github.com>
2026-02-15 23:24:45 -0600

    Initial Commit

---
+++ BearcatLib/BearcatLibApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearcatLibApp.swift
+//  BearcatLib
+//
+//  Created by Joseph Musenge on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct BearcatLibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
